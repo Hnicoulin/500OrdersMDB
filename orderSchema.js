efcefe158025d5d381c5e0a9c4556f5682b49ac7
bcuser <hmn180nlwh@gmail.com>
2022-04-17 13:35:50 -0700

    Added interesting querey button

---
+++ orderSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    StoreID: {
+        type: String,
+        required: true
+    },
+    SalesPersonID: {
+        type: String,
+        required: true
+    },
+    CdID: {
+        type: String,
+        required: true
+    },
+    PricePaid: {
+        type: String,
+        required: true
+    },
+    Time: {
+        type: Date,
+        required: true
+    }
+});
+
+  module.exports = mongoose.model("CdCollection", OrderSchema);
+
+
